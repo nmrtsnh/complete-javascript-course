c7c835b6f4c3ac6a0f1a0f63cf43e1f01e3062d4
nmrtsnh <namratasinha26@gmail.com>
2023-03-03 21:55:28 +0100

    update

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,7 @@
+console.log("me");
+
+var country = "Germany";
+var continent = "Europe";
+var population = 83.2;
+
+console.log(country, continent, population);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,24 @@
+let js = "amazing";
+
+// if (js === "amazing") alert("JavaScript is FUN");
+
+// console.log(40 + 8 + 30 - 20);
+
+// console.log("Jonas");
+// console.log(23);
+
+let firstName = "Matilda";
+
+// console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let pi = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
