7d4bcbfa903d4748450e464d4df783c2f4f4e6da
nmrtsnh <namratasinha26@gmail.com>
2023-03-03 22:36:50 +0100

    update coding challenge 2

--- 01-Fundamentals-Part-1/starter/assignmentAndChallenge.js
+++ 01-Fundamentals-Part-1/starter/assignmentAndChallenge.js
@@ -4,28 +4,14 @@ var population = 83.2;
 
 // console.log(country, continent, population);
 
+/*
 // Coding Challenge 1
 
-// const markWeight = 78;
-// const markHeight = 1.69;
+const markWeight = 78;
+const markHeight = 1.69;
 
-// const johnWeight = 92;
-// const johnHeight = 1.95;
-
-// const markBMI = markWeight / markHeight ** 2;
-// console.log(markBMI);
-
-// const johnBMI = johnWeight / johnHeight ** 2;
-
-// let markHigherBMI = true;
-
-// console.log(markBMI < johnBMI);
-
-const markWeight = 95;
-const markHeight = 1.88;
-
-const johnWeight = 85;
-const johnHeight = 1.76;
+const johnWeight = 92;
+const johnHeight = 1.95;
 
 const markBMI = markWeight / markHeight ** 2;
 console.log(markBMI);
@@ -35,3 +21,43 @@ const johnBMI = johnWeight / johnHeight ** 2;
 let markHigherBMI = true;
 
 console.log(markBMI < johnBMI);
+
+const markWeight1 = 95;
+const markHeight1 = 1.88;
+
+const johnWeight1 = 85;
+const johnHeight1 = 1.76;
+
+const markBMI1 = markWeight1 / markHeight1 ** 2;
+console.log(markBMI);
+
+const johnBMI1 = johnWeight1 / johnHeight1 ** 2;
+
+let markHigherBMI1 = true;
+
+console.log(markBMI1 < johnBMI1);
+
+*/
+
+/*
+
+// Coding Challenge 2
+
+const markWeight = 78;
+const markHeight = 1.69;
+
+const johnWeight = 92;
+const johnHeight = 1.95;
+
+const markBMI = markWeight / markHeight ** 2;
+console.log(markBMI);
+
+const johnBMI = johnWeight / johnHeight ** 2;
+
+if (markBMI > johnBMI) {
+  console.log(`Mark's BMI ${markBMI} is higher than John's ${johnBMI}`);
+} else {
+  console.log(`John's BMI ${johnBMI} is higher than Mark's ${markBMI}`);
+}
+
+*/
