e5fe8dc66a877166fcc521b07b982f3d834e301e
nmrtsnh <namratasinha26@gmail.com>
2023-03-05 23:31:54 +0100

    update coding challenge 1 of functions

--- 01-Fundamentals-Part-1/starter/assignmentAndChallenge.js
+++ 01-Fundamentals-Part-1/starter/assignmentAndChallenge.js
@@ -116,6 +116,6 @@ const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
 const total = bill + tip;
 
-console.log(
-  `The bill was ${bill}, the tip was ${tip}, and the total value ${total}`
-);
+// console.log(
+//   `The bill was ${bill}, the tip was ${tip}, and the total value ${total}`
+// );
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,4 +1,24 @@
 "use strict";
 
 const calcAverage = (a, b, c) => (a + b + c) / 3;
-console.log(1, 2, 3);
+
+let scoreDolphins = calcAverage(44, 23, 71);
+let scoreKoalas = calcAverage(65, 54, 49);
+console.log(scoreDolphins, scoreKoalas);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win (${avgDolphins} vs ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas wins (${avgKoalas} vs ${avgDolphins})`);
+  } else {
+    console.log("No team wins");
+  }
+};
+
+checkWinner(scoreDolphins, scoreKoalas);
+
+scoreDolphins = calcAverage(85, 54, 41);
+scoreKoalas = calcAverage(23, 34, 27);
+console.log(scoreDolphins, scoreKoalas);
+checkWinner(scoreDolphins, scoreKoalas);
