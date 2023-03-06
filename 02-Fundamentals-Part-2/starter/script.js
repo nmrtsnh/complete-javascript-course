2d8664e5ed4d9e78137716abbe75939533888824
nmrtsnh <namratasinha26@gmail.com>
2023-03-06 21:05:25 +0100

    update challenge

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -45,14 +45,42 @@ console.log(bills, tips, total);
 
 // Jonas has 3 friends, and his best friend is called Michael
 
+// const jonas = {
+//   firstName: "Jonas",
+//   lastName: "Schmedtmann",
+//   birthYear: 1991,
+//   job: "teacher",
+//   friends: ["Michael", "Peter", "Steven"],
+// };
+
+// console.log(
+//   ` ${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]} `
+// );
+
+// Challenge
+
+// Jonas is a 46- year old teacher, and he has a driver's licence
+
 const jonas = {
   firstName: "Jonas",
   lastName: "Schmedtmann",
-  age: 2037 - 1991,
+  birthYear: 1991,
   job: "teacher",
   friends: ["Michael", "Peter", "Steven"],
+  hasDriversLicence: true,
+
+  calcAge: function () {
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
+
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()} year old ${
+      jonas.job
+    }, and he has ${this.hasDriversLicence ? "a" : "not"} driver's licence`;
+  },
 };
 
-console.log(
-  ` ${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]} `
-);
+console.log(jonas.calcAge());
+
+console.log(jonas.getSummary());
