2875990cb893548d15fc48c22624ca9bff4f401b
nmrtsnh <namratasinha26@gmail.com>
2023-03-16 16:41:12 +0100

    update guess my number project

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -13,7 +13,7 @@ console.log(document.querySelector('.guess').value);
 */
 
 let secretNumber = Math.floor(Math.random() * 20) + 1;
-
+let highscore = 0;
 let score = 20;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -29,6 +29,11 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
   }
   // When the guess is too high
   else if (guess > secretNumber) {
