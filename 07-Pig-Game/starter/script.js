7b6ee90e4f07420bcde36c63f06f3d6cfccc3886
nmrtsnh <namratasinha26@gmail.com>
2023-03-20 14:00:12 +0100

    update

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 //Selecting Element
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0 = document.getElementById('score--0');
 const score1 = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -16,7 +18,18 @@ score0.textContent = 0;
 score1.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
 //Rollig Dice functionality
 btnRoll.addEventListener('click', function () {
   // 1. Generating a random dice roll
@@ -30,8 +43,22 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     //Add dice roll to current score
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     //Switch Player
+    switchPlayer();
   }
+});
+
+btnHold.addEventListener('click', function () {
+  //1. Add current score to active player's score
+  scores[activePlayer] += currentScore;
+  document.getElementById(`current--${activePlayer}`).textContent =
+    scores[activePlayer];
+  //2. Check if player's score is >=100
+  //Finish the game
+
+  //Switch to next player
+  switchPlayer();
 });
